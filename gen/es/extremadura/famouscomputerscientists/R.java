0d74fa4be5b2b0ffc7359c8d3bfbe36730818a5f
Rafael J Garcia <lestatmer@gmail.com>
2013-12-27 16:55:54 +0100

    famous: Añadido todo el texto en Linus y Richard y puesto scroll en ellos.

--- gen/es/extremadura/famouscomputerscientists/R.java
+++ gen/es/extremadura/famouscomputerscientists/R.java
@@ -26,9 +26,10 @@ public final class R {
         public static final int richard_stallman=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int contenido=0x7f080002;
+        public static final int action_settings=0x7f080004;
+        public static final int contenido=0x7f080003;
         public static final int imageView1=0x7f080001;
+        public static final int scroll=0x7f080002;
         public static final int titulo=0x7f080000;
     }
     public static final class layout {
